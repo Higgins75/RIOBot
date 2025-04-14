f16b00fc6ba9a3a7cfe8511d5ea2f5dcaf68506c
Higgins75 <danielmhiggins75@gmail.com>
2025-04-14 13:56:20 +0100

    Created basic raidlink functionality Added API link for future uses

---
+++ API_Call.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext import commands
+import requests
+
+intents = discord.Intents.all()
+intents.members = True
+intents.message_content = True
+bot = commands.Bot(command_prefix='.',intents=intents)
+
+response = requests.get('https://raider.io/api/v1/characters/profile?region=eu&realm=draenor&name=gigdh')
+#print(response.json())
+
+def GetRioLink():
+    print("GetLinkWorking")
+    return "https://raider.io/characters/"
--- Main.py
+++ Main.py
@@ -1,15 +1,12 @@
 import discord
-from discord import app_commands
 from discord.ext import commands
 from Bot_Token import token
+from API_Call import GetRioLink
 
 intents = discord.Intents.all()
 intents.members = True
 intents.message_content = True
 bot = commands.Bot(command_prefix='.',intents=intents)
-
-
-
 
 @bot.event
 async def on_ready():
@@ -20,8 +17,13 @@ async def on_ready():
     except Exception as e:
         print(e)
 
-@bot.tree.command(name = "hello")
+@bot.tree.command(name="hello")
 async def hello(interaction: discord.Interaction):
     await interaction.response.send_message(f"Hey {interaction.user.mention}!")
+    
+@bot.tree.command(name="raidlink")
+async def raiderurl(interaction: discord.Interaction, region : str, realm: str, charactername: str):
+    RaiderLinkURL = GetRioLink() + region + '/' + realm + '/' + charactername
+    await interaction.response.send_message(f'Your RIO link is {RaiderLinkURL}')
         
 bot.run(token)
