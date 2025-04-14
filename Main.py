934adf4c955fd95105b4dac8afac5e5f6070c9c1
Higgins75 <danielmhiggins75@gmail.com>
2025-04-14 12:08:20 +0100

    Added / command functionality to the bot

--- Main.py
+++ Main.py
@@ -1,22 +1,27 @@
 import discord
+from discord import app_commands
+from discord.ext import commands
 from Bot_Token import token
 
-
-intents = discord.Intents.default()
+intents = discord.Intents.all()
+intents.members = True
 intents.message_content = True
+bot = commands.Bot(command_prefix='.',intents=intents)
 
-client = discord.Client(intents=intents)
 
-@client.event
+
+
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f'We have logged in as {bot.user}')
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} command(s)")
+    except Exception as e:
+        print(e)
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-
-client.run(token)
+@bot.tree.command(name = "hello")
+async def hello(interaction: discord.Interaction):
+    await interaction.response.send_message(f"Hey {interaction.user.mention}!")
+        
+bot.run(token)
