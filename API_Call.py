d5b1fb3119c61dfd27032e0170b3b4b120459f91
Higgins75 <danielmhiggins75@gmail.com>
2025-04-14 14:09:45 +0100

    Streamlined API_Call functions

--- API_Call.py
+++ API_Call.py
@@ -1,11 +1,6 @@
-import discord
-from discord.ext import commands
+import json
 import requests
 
-intents = discord.Intents.all()
-intents.members = True
-intents.message_content = True
-bot = commands.Bot(command_prefix='.',intents=intents)
 
 response = requests.get('https://raider.io/api/v1/characters/profile?region=eu&realm=draenor&name=gigdh')
 #print(response.json())
