0208cfbfda333a94f597009f18fb6f5d73091c96
Higgins75 <danielmhiggins75@gmail.com>
2025-04-15 17:33:11 +0100

    Minor update to syntax

--- API_Call.py
+++ API_Call.py
@@ -8,13 +8,7 @@ def GetRioLink(region : str, realm: str, charactername: str):
 
 def GetRIO(region : str, realm: str, charactername: str):
     RequestURL = 'https://raider.io/api/v1/characters/profile?region=' + region + '&realm=' + realm + '&name=' + charactername + '&fields=mythic_plus_scores_by_season%3Aseason-tww-2'
-    response = requests.get(RequestURL)
-    
-    # For troubleshooting, print status code
-    # print (response.status_code)
-    
     info = requests.get(RequestURL).json()
-    #print(info["mythic_plus_scores_by_season"]['scores'])
     info_keys = (info["mythic_plus_scores_by_season"][0])
     scores = info_keys["scores"]
     return (scores['all'])
