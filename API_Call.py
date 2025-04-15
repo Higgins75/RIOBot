57b99b7abe258772fca0fb5dd68f396ef8998491
Higgins75 <danielmhiggins75@gmail.com>
2025-04-15 02:31:40 +0100

    Added RIO Command

--- API_Call.py
+++ API_Call.py
@@ -2,9 +2,19 @@ import json
 import requests
 
 
-response = requests.get('https://raider.io/api/v1/characters/profile?region=eu&realm=draenor&name=gigdh')
-#print(response.json())
-
 def GetRioLink(region : str, realm: str, charactername: str):
     RaiderLinkURL = "https://raider.io/characters/" + region + '/' + realm + '/' + charactername
     return RaiderLinkURL
+
+def GetRIO(region : str, realm: str, charactername: str):
+    RequestURL = 'https://raider.io/api/v1/characters/profile?region=' + region + '&realm=' + realm + '&name=' + charactername + '&fields=mythic_plus_scores_by_season%3Aseason-tww-2'
+    response = requests.get(RequestURL)
+    
+    # For troubleshooting, print status code
+    # print (response.status_code)
+    
+    info = requests.get(RequestURL).json()
+    #print(info["mythic_plus_scores_by_season"]['scores'])
+    info_keys = (info["mythic_plus_scores_by_season"][0])
+    scores = info_keys["scores"]
+    return (scores['all'])
--- Main.py
+++ Main.py
@@ -1,8 +1,11 @@
 import discord
+import json
+import requests
 from typing import Literal
 from discord.ext import commands
 from Bot_Token import token
 from API_Call import GetRioLink
+from API_Call import GetRIO
 from Lists import regions
 
 intents = discord.Intents.all()
@@ -24,8 +27,14 @@ async def hello(interaction: discord.Interaction):
     await interaction.response.send_message(f"Hey {interaction.user.mention}!")
     
 @bot.tree.command(name="raidlink")
-async def raiderurl(interaction: discord.Interaction, region : Literal[regions], realm: str, charactername: str):
+async def raidlink(interaction: discord.Interaction, region : Literal[regions], realm: str, charactername: str):
     RaiderLinkURL = GetRioLink(region, realm, charactername)
     await interaction.response.send_message(f'Your RIO link is {RaiderLinkURL}')
+    
+@bot.tree.command(name='rio')
+async def rio(interaction: discord.Interaction, region : Literal[regions], realm: str, charactername: str):
+    Rio_Total = GetRIO(region, realm, charactername)
+    await interaction.response.send_message(f'The Raider.IO Score of {charactername} is {Rio_Total}')
+    
         
 bot.run(token)
