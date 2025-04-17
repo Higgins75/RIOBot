74b4ede571be25dd579fd0e23dfc3e58ec1d6adc
Higgins75 <danielmhiggins75@gmail.com>
2025-04-17 09:34:00 +0100

    Added comments to main and started SQL Dev

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 import discord
+import sqlite3
 import json
 import requests
 from typing import Literal
@@ -9,11 +10,16 @@ from API_Call import GetRIO
 from API_Call import GetBestRuns
 from Lists import regions
 
+#Sets up Discord
 intents = discord.Intents.all()
 intents.members = True
 intents.message_content = True
 bot = commands.Bot(command_prefix='.',intents=intents)
 
+#calls the SQL Database associated with this file
+con = sqlite3.connect("player_data.db")
+
+#Readys the Bot, prints messages to CLI to allow for Command Debug
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
@@ -23,23 +29,29 @@ async def on_ready():
     except Exception as e:
         print(e)
 
-@bot.tree.command(name="hello")
-async def hello(interaction: discord.Interaction):
+#General Test Command, currently replies 'Hey @user'
+@bot.tree.command(name="tests")
+async def test(interaction: discord.Interaction):
+
     await interaction.response.send_message(f"Hey {interaction.user.mention}!")
     
+#Command to reply with a users Raider.IO Link - Requires Error catching    
 @bot.tree.command(name="raidlink")
 async def raidlink(interaction: discord.Interaction, region : Literal[regions], realm: str, charactername: str):
     RaiderLinkURL = GetRioLink(region, realm, charactername)
     await interaction.response.send_message(f'Your RIO link is {RaiderLinkURL}')
-    
+
+#Command to return a users Raider.IO Score for the current season    
 @bot.tree.command(name='rio')
 async def rio(interaction: discord.Interaction, region : Literal[regions], realm: str, charactername: str):
     Rio_Total = GetRIO(region, realm, charactername)
     await interaction.response.send_message(f'The Raider.IO Score of {charactername} is {Rio_Total}')
-    
+
+#Command in progress of development.    
 @bot.tree.command(name='best_runs')
 async def runs(interaction: discord.Interaction, region : Literal[regions], realm: str, charactername: str):
     lowest_key = GetBestRuns(region, realm, charactername)
     await interaction.response.send_message(f'The minimum key of {charactername} is {lowest_key}')
-        
+
+#runs the bot        
 bot.run(token)
